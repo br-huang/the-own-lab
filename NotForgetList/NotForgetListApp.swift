3402c75e694242e072bb4a94d72628e5a728e770
brian huang <ronggg27825@gmail.com>
2026-04-09 11:28:26 +0800

    Initial Commit

---
+++ NotForgetList/NotForgetListApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotForgetListApp.swift
+//  NotForgetList
+//
+//  Created by Brian Huang on 4/9/26.
+//
+
+import SwiftUI
+
+@main
+struct NotForgetListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
