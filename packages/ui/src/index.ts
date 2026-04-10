0a4442f5095d025d0763e0347d46b289d0fc4b5b
Brian <ronggg27825@gmail.com>
2026-04-10 09:55:48 +0800

    feat(ui): add shadcn core components

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,63 @@
+export { cn } from "./lib/utils";
+export { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "./components/ui/accordion";
+export { Alert, AlertDescription, AlertTitle } from "./components/ui/alert";
+export { Avatar, AvatarFallback, AvatarImage } from "./components/ui/avatar";
+export { Badge, badgeVariants } from "./components/ui/badge";
+export { Button, buttonVariants } from "./components/ui/button";
+export {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./components/ui/card";
+export { Checkbox } from "./components/ui/checkbox";
+export {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogOverlay,
+  DialogPortal,
+  DialogTitle,
+  DialogTrigger,
+} from "./components/ui/dialog";
+export {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "./components/ui/dropdown-menu";
+export { Input } from "./components/ui/input";
+export { Label } from "./components/ui/label";
+export { Popover, PopoverAnchor, PopoverContent, PopoverTrigger } from "./components/ui/popover";
+export {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectSeparator,
+  SelectTrigger,
+  SelectValue,
+} from "./components/ui/select";
+export { Separator } from "./components/ui/separator";
+export { Skeleton } from "./components/ui/skeleton";
+export { Switch } from "./components/ui/switch";
+export { Tabs, TabsContent, TabsList, TabsTrigger } from "./components/ui/tabs";
+export { Textarea } from "./components/ui/textarea";
+export { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "./components/ui/tooltip";
